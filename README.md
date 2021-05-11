a2d41609e4a21f678ca70e55dfd70beed3337b72
Arnav Jain <arnjain3@gmail.com>
2021-05-11 23:43:46 +0530

    Solved datetime.now, added delta(unstable)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 <h1> Exact Time </h1>
 
-It finds the exact time if time zones were not a thing. 00:00 is GMT only.
+It finds the exact time if time zones were not a thing. The differnce is taken from GMT.
+
+It give the diffrence between actual time(according to time zone) and exact time(from calculation) and provides a delta between them.
--- app.py
+++ app.py
@@ -1,24 +1,41 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone, tzinfo
 from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
+import pytz
 
 location = input("Enter a location: ")
 
-# def main():
-#     getLocation(location)
-#     calc()
+tzf = TimezoneFinder()
+
+def main():
+    calc()
 
 def getLocation(location):
     loc = Nominatim(user_agent="getLoc")
     getLoc = loc.geocode(location)
     long = getLoc.longitude
-    #print(long)
-    return long
+    lati = getLoc.latitude
+    return long, lati
+
+def timeZone():
+    long, lati = getLocation(location)
+    timezone = tzf.timezone_at(lng=long, lat=lati)
+    tz = pytz.timezone(timezone)
+    return tz
 
 def calc():
-    long = getLocation(location)
+    long, lati = getLocation(location)
+    tz = timeZone()
     min = long / 0.25
     exactTime = datetime.utcnow() + timedelta(minutes=min)
-    print(exactTime)
+    currentTime = datetime.now(tz).replace(tzinfo=None)
+    
+    print("Time Zone:", tz)
+    print("\nThe exact time is: " ,exactTime)
+    print("The actual time is: ", currentTime, "\n")
+    
+    delta = exactTime - currentTime
+    print("The diffrence is:", delta)
 
 
-calc()
+main()
