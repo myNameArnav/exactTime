496dc8e911ca86b5a078453e4999437d028b5713
Arnav Jain <35961071+myNameArnav@users.noreply.github.com>
2022-06-14 01:04:11 +0530

    delta is working

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from datetime import datetime, timedelta, timezone, tzinfo
+from datetime import datetime, timedelta
 from geopy.geocoders import Nominatim
 from timezonefinder import TimezoneFinder
 import pytz
 
-location = input("Enter a location: ")
+location = input("Enter a location (It can be a city, a state or even a zip code): ")
 
 tzf = TimezoneFinder()
 
+
 def main():
     calc()
+
 
 def getLocation(location):
     loc = Nominatim(user_agent="getLoc")
@@ -17,25 +19,33 @@ def getLocation(location):
     lati = getLoc.latitude
     return long, lati
 
+
 def timeZone():
     long, lati = getLocation(location)
     timezone = tzf.timezone_at(lng=long, lat=lati)
     tz = pytz.timezone(timezone)
     return tz
 
+
 def calc():
     long, lati = getLocation(location)
     tz = timeZone()
     min = long / 0.25
     exactTime = datetime.utcnow() + timedelta(minutes=min)
-    currentTime = datetime.now(tz).replace(tzinfo=None)
-    
+    actualTime = datetime.now(tz).replace(tzinfo=None)
+
     print("Time Zone:", tz)
-    print("\nThe exact time is: " ,exactTime)
-    print("The actual time is: ", currentTime, "\n")
-    
-    delta = exactTime - currentTime
-    print("The diffrence is:", delta)
+    print("\nThe exact time is: ", exactTime)
+    print("The actual time is: ", actualTime, "\n")
+
+    if exactTime > actualTime:
+        delta = exactTime - actualTime
+        sign = "+"
+    else:
+        delta = actualTime - exactTime
+        sign = "-"
+
+    print("The difference is: ", sign, delta)
 
 
 main()
